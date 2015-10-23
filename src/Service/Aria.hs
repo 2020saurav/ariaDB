05c9249315f3bf57b0f0225eb291f611489a5221
Saurav Kumar <2020saurav@gmail.com>
2015-10-24 00:03:52 +0530

    file IO test code: Unary Tree insert

--- src/Service/Aria.hs
+++ src/Service/Aria.hs
@@ -7,7 +7,7 @@ import qualified Data.Text as T (unpack)
 type AriaKey    = String
 type AriaValue  = String
 
-data AriaKV = AriaKV {key :: AriaKey, value :: AriaValue} deriving Show
+data AriaKV = AriaKV {key :: AriaKey, value :: AriaValue} deriving (Show, Read)
 
 ariaKey AriaKV {key=k, value=_} = k
 ariaValue AriaKV {key=_, value=v} = v
---
+++ src/Service/UnaryTree.hs
@@ -0,0 +1,30 @@
+-- This code is used to test file IO and its interaction with the Warp API.
+-- Actual code will have B+ Tree implementation. This code is very simplistic,
+-- but covers all important aspects (walls) that we will hit in the project.
+-- BPlusTree module will export same functions as this and no changes should be
+-- made anywhere else. Just indexing tree will shift from UTree to BPTree
+
+module UnaryTree where -- TODO write exports
+
+import Aria
+import qualified Data.ByteString.Char8 as B
+
+type UTree = [AriaKV]
+type TempAriaKey = Char
+
+dbFileName :: FilePath
+dbFileName = "testdb"
+-- The file must not be nil. Atleast put "[]"
+
+db :: IO UTree
+db = do
+    contents <- B.readFile dbFileName
+    return (read (B.unpack contents) :: UTree)
+
+-- This implementation will just ignore the key for now
+insert :: AriaKey -> AriaValue -> IO ()
+insert key value = do
+    utree <- db
+    let newutree = utree ++ [AriaKV key value]
+    B.writeFile dbFileName . B.pack . show $ newutree
+    return ()
